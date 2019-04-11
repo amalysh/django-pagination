278f8f48de7ae1205eced958b721bdfc0c58bb9a
Chris Mytton <chrismytton@gmail.com>
2019-04-11 10:20:53 +0100

    Update tests/runtests.py to work with newer Django versions

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,16 +1,27 @@
 import sys
-sys.path.append('..')
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
 
-import os
-# Make a backup of DJANGO_SETTINGS_MODULE environment variable to restore later.
-backup = os.environ.get('DJANGO_SETTINGS_MODULE', '')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-from django.test.simple import run_tests
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+            }
+        },
+        INSTALLED_APPS=(
+            'pagination',
+        ),
+        SITE_ID=1,
+        SECRET_KEY='this-is-just-for-tests-so-not-that-secret',
+        ROOT_URLCONF='',
+    )
 
 if __name__ == "__main__":
-    failures = run_tests(['pagination',], verbosity=9)
+    django.setup()
+    test_runner = DiscoverRunner(verbosity=1)
+    failures = test_runner.run_tests(['pagination'])
     if failures:
         sys.exit(failures)
-    # Reset the DJANGO_SETTINGS_MODULE to what it was before running tests.
-    os.environ['DJANGO_SETTINGS_MODULE'] = backup
